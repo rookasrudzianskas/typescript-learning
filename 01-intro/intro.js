efd7a9cf807b8ef21d40fbe64b4dadbeb2490a4d
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 11:52:40 +0200

    Rokas

---
+++ 01-intro/intro.js
@@ -0,0 +1,4 @@
+var user = { name: "Rokas", age: 10 };
+console.log(user);
+console.log(user.name);
+//# sourceMappingURL=intro.js.map
---
+++ 01-intro/intro.ts
@@ -0,0 +1,4 @@
+let user = { name: "Rokas", age: 10};
+
+console.log(user);
+console.log(user.name);

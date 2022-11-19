dc867afe2d6b45994b3a7955b7e04ce4e28f5b05
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 12:48:29 +0200

    Rokas

--- 05-functions/functions.js
+++ 05-functions/functions.js
@@ -2,8 +2,8 @@
 exports.__esModule = true;
 function addTwo(num) {
     // num.toUpperCase(); // Error
-    // return num + 2;
-    return "hello";
+    return num + 2;
+    // return "hello";
 }
 function getUpper(val) {
     return val.toUpperCase();
@@ -17,4 +17,25 @@ getUpper("Rokas"); // OK
 addTwo(2); // 4
 signUpUser("Rokas", "rokas.rudzenskas@gmail.com", false); // OK
 loginUser("Rokas", "hello@hello.com"); // OK
+// Path: 05-functions/functions.ts
+// function getValue(myVal: number): number {
+//     if(myVal > 5) {
+//         return true;
+//     }
+//     return "200 OK";
+// }
+var getHello = function (s) {
+    return "";
+};
+var heros = ["Batman", "Superman", "Spiderman"];
+// const heros = [1, 2, 3];
+heros.map(function (hero) {
+    return "Hero is ".concat(hero);
+});
+function consoleError(errorMessage) {
+    console.log("Error", errorMessage);
+}
+function handleError(errorMessage) {
+    throw new Error(errorMessage);
+}
 //# sourceMappingURL=functions.js.map
--- 05-functions/functions.ts
+++ 05-functions/functions.ts
@@ -1,7 +1,7 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
     // num.toUpperCase(); // Error
-    // return num + 2;
-    return "hello";
+    return num + 2;
+    // return "hello";
 }
 
 function getUpper(val: string) {
@@ -17,5 +17,32 @@ getUpper("Rokas"); // OK
 addTwo(2); // 4
 signUpUser("Rokas", "rokas.rudzenskas@gmail.com", false); // OK
 loginUser("Rokas", "hello@hello.com"); // OK
+
+// Path: 05-functions/functions.ts
+// function getValue(myVal: number): number {
+//     if(myVal > 5) {
+//         return true;
+//     }
+//     return "200 OK";
+// }
+
+const getHello = (s: string): string => {
+    return "";
+}
+
+const heros = ["Batman", "Superman", "Spiderman"];
+// const heros = [1, 2, 3];
+
+heros.map((hero): string => {
+    return `Hero is ${hero}`;
+});
+
+function consoleError(errorMessage: string): void {
+    console.log("Error", errorMessage);
+}
+
+function handleError(errorMessage: string): never {
+    throw new Error(errorMessage);
+}
 
 export {};

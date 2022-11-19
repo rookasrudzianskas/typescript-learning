d75f62853ee037362db2b3929b6afc9d2dd04fa3
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 16:15:29 +0200

    Rokas

--- typescript-real-project/dist/index.js
+++ typescript-real-project/dist/index.js
@@ -1,11 +1,20 @@
 "use strict";
-class User {
-    constructor(email, name) {
-        this.city = "New York";
-        this.email = email;
-        this.name = name;
-    }
-}
-const rokas = new User("h@h.com", "Rokas");
+// class User {
+//     public email: string;
+//     private name: string;
+//     readonly city: string = "New York";
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+// const rokas = new User("h@h.com","Rokas");
 // rokas.city = "Delhi"; // Error: Cannot assign to 'city' because it is a read-only property.
 // rokas.city = 4; // Error
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "New York";
+    }
+}
--- typescript-real-project/src/index.ts
+++ typescript-real-project/src/index.ts
@@ -1,13 +1,24 @@
-class User {
-    email: string;
-    name: string;
-    readonly city: string = "New York";
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
-    }
-}
+// class User {
+//     public email: string;
+//     private name: string;
+//     readonly city: string = "New York";
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
 
-const rokas = new User("h@h.com","Rokas");
+// const rokas = new User("h@h.com","Rokas");
 // rokas.city = "Delhi"; // Error: Cannot assign to 'city' because it is a read-only property.
 // rokas.city = 4; // Error
+
+class User {
+    readonly city: string = "New York";
+    constructor(
+        public email: string,
+        public name: string,
+        // private userId: string,
+    ) {
+
+    }
+}

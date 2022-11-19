6d195df293bdbaaaa93046eeab6e777932e80e5c
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 18:21:25 +0200

    Add type guard to getArea function
    The type guard is used to narrow the type of the shape parameter
    to either Circle or Square.

--- typescript-real-project/dist/detection.js
+++ typescript-real-project/dist/detection.js
@@ -61,3 +61,17 @@ function getFood(pet) {
         return "bird food";
     }
 }
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+    }
+}
--- typescript-real-project/src/detection.ts
+++ typescript-real-project/src/detection.ts
@@ -87,6 +87,7 @@ function getFood(pet: Fish | Bird) {
     }
 }
 
+// u can use the type guard in the function
 interface Circle {
     kind: "circle";
     radius: number;
@@ -103,6 +104,23 @@ interface Rectangle {
     width: number;
 }
 
+type Shape = Circle | Square;
+
+function getTrueShape(shape: Shape) {
+    if(shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+    }
+}
 
 
 

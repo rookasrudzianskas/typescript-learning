2a5d470d14d7309cbab5e74f5bc53f75ea32ac86
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 16:07:36 +0200

    Rokas

--- typescript-real-project/dist/index.js
+++ typescript-real-project/dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
-console.log('Hello World still');
-alert('Hello World!');
+class User {
+    constructor(email, name) {
+        this.city = "New York";
+        this.email = email;
+        this.name = name;
+    }
+}
+const rokas = new User("h@h.com", "Rokas");
+// rokas.city = "Delhi"; // Error: Cannot assign to 'city' because it is a read-only property.
+// rokas.city = 4; // Error
--- typescript-real-project/src/index.ts
+++ typescript-real-project/src/index.ts
@@ -1,2 +1,13 @@
-console.log('Hello World still');
-alert('Hello World!');
+class User {
+    email: string;
+    name: string;
+    readonly city: string = "New York";
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
+
+const rokas = new User("h@h.com","Rokas");
+// rokas.city = "Delhi"; // Error: Cannot assign to 'city' because it is a read-only property.
+// rokas.city = 4; // Error

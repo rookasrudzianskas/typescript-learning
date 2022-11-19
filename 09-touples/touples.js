c17ac7bd478b6d6d00e91cab851d19de7f9f662e
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 14:43:18 +0200

    Rokas

--- 09-touples/touples.js
+++ 09-touples/touples.js
@@ -1,2 +1,8 @@
-var user = ['chris', 1];
+// const user: (string | number)[] = ['chris', 1];
+var tUser;
+tUser = ['chris', 1, true];
+var rgb = [255, 255, 255];
+var newUser = [1112, "rokas@gmail.com"];
+newUser[1] = "rokas@gmail.com";
+// newUser.push(true); // error
 //# sourceMappingURL=touples.js.map
--- 09-touples/touples.ts
+++ 09-touples/touples.ts
@@ -1,1 +1,15 @@
-const user: (string | number)[] = ['chris', 1];
+// const user: (string | number)[] = ['chris', 1];
+let tUser: [string, number, boolean];
+
+tUser = ['chris', 1, true];
+
+let rgb: [number, number, number] = [255, 255, 255];
+
+type User = [number, string];
+
+const newUser: User = [1112, "rokas@gmail.com"];
+
+newUser[1] = "rokas@gmail.com"
+// newUser.push(true); // error
+
+

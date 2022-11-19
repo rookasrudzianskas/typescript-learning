1ba0f5acf5b539c0ae4fc4536e210635c133694a
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 15:04:51 +0200

    Rokas

--- 10-interfaces/interfaces.js
+++ 10-interfaces/interfaces.js
@@ -1,6 +1,7 @@
 // Hey does pilot work?
 // Do you work ok?
 // GitHub are you working???
+// Any help? Bro??? Bro? Any help? Thanks bro.
 var rokas = { dbId: 22, email: "rokas@rokas.com", userId: 2211,
     startTrail: function () {
     }
--- 10-interfaces/interfaces.ts
+++ 10-interfaces/interfaces.ts
@@ -9,6 +9,7 @@ interface User {
 // Hey does pilot work?
 // Do you work ok?
 // GitHub are you working???
+// Any help? Bro??? Bro? Any help? Thanks bro.
 
 const rokas: User = {dbId: 22, email: "rokas@rokas.com", userId: 2211,
     startTrail: () => {

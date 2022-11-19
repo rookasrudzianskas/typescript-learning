3989742bb5ea2b0d26379c31ab1a51830ac89e73
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 17:42:37 +0200

    Rokas

--- typescript-real-project/dist/detection.js
+++ typescript-real-project/dist/detection.js
@@ -3,4 +3,12 @@ function detectTypes(val) {
     if (typeof val === "string") {
         return val.toLowerCase();
     }
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log("No id provided");
+        return;
+    }
+    id.toLowerCase();
 }
--- typescript-real-project/src/detection.ts
+++ typescript-real-project/src/detection.ts
@@ -2,4 +2,13 @@ function detectTypes(val: number | string) {
     if(typeof val === "string") {
         return val.toLowerCase();
     }
+    return val + 3;
+}
+
+function provideId(id: string | null) {
+    if(!id) {
+        console.log("No id provided");
+        return;
+    }
+    id.toLowerCase();
 }
